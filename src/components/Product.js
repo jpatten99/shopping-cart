eddf7422ca0d50cc6e0225c16274679c004a0db1
Joshua <joshuagpatten@gmail.com>
2023-07-02 14:23:16 -0400

    Add increment/decrement functionality

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,14 +4,38 @@ import "../Styles/Product.css"
 
 
 function Product(props) {
+  function decrement(e) {
+    let self = e.target
+    //console.log(self)
+    let target = document.getElementById(`input${self.id}`)
+    console.log(target.value)
+    if(isNaN(target.value)) {
+      target.value = 0
+    }
+    else {
+      target.value --
+    }
+  }
+
+  function increment(e) {
+    let self = e.target
+    //console.log(self)
+    let target = document.getElementById(`input${self.id}`)
+    console.log(target.value)
+    if(isNaN(target.value)) {
+      target.value = 0
+    }
+    else {
+      target.value ++
+    }
+  }
   return (
     <div className="Product-Container">
       <h4 id="Product-Title">{props.Title}</h4>
       <img src={props.src}alt="Product" className="Product-Img"></img>
-      <button id="decrement-button">-</button>
-      <input type="text" placeholder="0" id="Product-input"></input>
-      <button id="increment-button">+</button>
-    
+      <button className="decrement-button" onClick={decrement} id={props.myid}>-</button>
+      <input type="text" placeholder="0" className="Product-input" id={`input${props.myid}`}></input>
+      <button className="increment-button" onClick={increment} id={props.myid}>+</button>
     </div>
   )
 }
